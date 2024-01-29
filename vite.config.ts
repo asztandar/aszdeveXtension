d17f511b519590e33fd34456a389e80bc0cc8e50
asztandar <sztandaradam@gmail.com>
2024-01-29 23:03:58 +0100

    removed unused dep.

--- vite.config.ts
+++ vite.config.ts
@@ -1,14 +1,10 @@
 import { defineConfig } from 'vite'
 import react from '@vitejs/plugin-react'
-import hotReloadExtension from 'hot-reload-extension-vite';
 
 // https://vitejs.dev/config/
 export default defineConfig({
   plugins: [
     react(),
-    hotReloadExtension({
-      log: true,
-      backgroundPath: 'path/to/background' // relative path to background script file
-    })
+    
   ],
 })
