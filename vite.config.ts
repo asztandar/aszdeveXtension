40c6d87769f6ea09a0aac4165462da74ceccbc8d
asztandar <sztandaradam@gmail.com>
2024-01-29 22:33:04 +0100

    added hot reload

--- vite.config.ts
+++ vite.config.ts
@@ -1,7 +1,14 @@
 import { defineConfig } from 'vite'
 import react from '@vitejs/plugin-react'
+import hotReloadExtension from 'hot-reload-extension-vite';
 
 // https://vitejs.dev/config/
 export default defineConfig({
-  plugins: [react()],
+  plugins: [
+    react(),
+    hotReloadExtension({
+      log: true,
+      backgroundPath: 'path/to/background' // relative path to background script file
+    })
+  ],
 })
