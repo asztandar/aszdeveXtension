c48995c15bb31c6a636c797738e4c3a5f67dcc2a
asztandar <sztandaradam@gmail.com>
2024-01-30 00:12:16 +0100

    added icons and styles

--- src/App.tsx
+++ src/App.tsx
@@ -1,33 +1,34 @@
-import { useState } from 'react'
-import reactLogo from './assets/react.svg'
-import viteLogo from '/vite.svg'
 import './App.css'
-
+import GridContainer from './components/GridContainer'
+import GridItem from './components/GridItem';
+import { CgColorPicker } from "react-icons/cg";
+import { FaListCheck } from "react-icons/fa6";
+import { BsRegex } from "react-icons/bs";
+import { SiConvertio } from "react-icons/si";
+import { IoKeyOutline } from "react-icons/io5";
+import { FaTextWidth } from "react-icons/fa6";
+import { SiProtractor } from "react-icons/si";
 function App() {
-  const [count, setCount] = useState(0)
+
+  const testClick = () => {
+    console.log("test");
+  }
 
   return (
     <>
       <div>
-        <a href="https://vitejs.dev" target="_blank">
-          <img src={viteLogo} className="logo" alt="Vite logo" />
-        </a>
-        <a href="https://react.dev" target="_blank">
-          <img src={reactLogo} className="logo react" alt="React logo" />
-        </a>
+        <h1>AszDevExtension:</h1>
+        <GridContainer>
+          <GridItem onclick={testClick}><CgColorPicker /></GridItem>
+          <GridItem onclick={testClick}><FaListCheck /></GridItem>
+          <GridItem onclick={testClick}><BsRegex /></GridItem>
+          <GridItem onclick={testClick}><SiConvertio /></GridItem>
+          <GridItem onclick={testClick}><IoKeyOutline /></GridItem>
+          <GridItem onclick={testClick}><FaTextWidth /></GridItem>
+          <GridItem onclick={testClick}><SiProtractor /></GridItem>
+
+        </GridContainer>
       </div>
-      <h1>Vite + React</h1>
-      <div className="card">
-        <button onClick={() => setCount((count) => count + 1)}>
-          count is {count}
-        </button>
-        <p>
-          Edit <code>src/App.tsx</code> and save to test HMR
-        </p>
-      </div>
-      <p className="read-the-docs">
-        Click on the Vite and React logos to learn more
-      </p>
     </>
   )
 }
